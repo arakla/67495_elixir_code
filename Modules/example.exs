409e82dea29479cb39b3960608dea379f9fec90f
Aamer <arakla@andrew.cmu.edu>
2016-06-03 00:03:18 -0400

    Updated slides with nested modules and added example code;

---
+++ Modules/example.exs
@@ -0,0 +1,5 @@
+defmodule Example do
+  def greeting(name) do
+    "Hello #{name}."
+  end
+end
---
+++ Modules/exampleGreeting.exs
@@ -0,0 +1,9 @@
+defmodule Example.Greetings do
+  def morning(name) do
+    "Good morning #{name}."
+  end
+
+  def evening(name) do
+    "Good night #{name}."
+  end
+end
